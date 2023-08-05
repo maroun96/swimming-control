cb91fd3cecef1b47d4fc83b98d21df82105c5be0
Karl Maroun <karlmaroun574@gmail.com>
2023-08-06 01:44:16 +0200

    organize data generation process

--- data_gen.py
+++ data_gen.py
@@ -4,25 +4,22 @@ from pathlib import Path
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 import petsc4py
 from petsc4py import PETSc
-from jenkspy import JenksNaturalBreaks
-from scipy.interpolate import PchipInterpolator
 
 from ns2d.utils.pyconfig import Config
 from ns2d.utils.export import ObsExporter
 
 from solver import NSSolver
-from helpers import SlidingWindow, get_phase
+from helpers import SlidingWindow, get_phase, construct_iterator, compute_dist, smoothstep 
 
 COMM = PETSc.COMM_WORLD
 RANK = COMM.Get_rank()
 CFG_PATH = Path("config.yml").resolve()
 MAIN_CFG = Config.from_yaml(config_path=CFG_PATH)
 RESULTS_DIR_PATH = Path(MAIN_CFG.export_info.results_dirpath).resolve()
-N_CLUSTERS = 4
+T_SMOOTH = 0.2 #add to config
 
 if __name__ == "__main__":
     petsc4py.init(sys.argv, comm=COMM)
@@ -31,13 +28,14 @@ if __name__ == "__main__":
     
     obs_exporter.add_exported_param(name=[
         "swimming_frequency",
+        "maximum_amplitude",
         "x",
         "y",
         "velocity_x",
         "velocity_y",
         "Cx",
         "Cy",
-        "density",
+        "power_def",
         "surface_area"
     ])
 
@@ -47,38 +45,43 @@ if __name__ == "__main__":
     ns_solver.add_exported_vec(vec=ns_solver.fields.current_time.local_pressure, vector_name="p")
     ns_solver.add_exported_vec(vec=ns_solver.fields.solid_ls[1].local_levelset_solid, vector_name="solid_level_set")
 
-    frequencies = [1.0, 1.5, 2.0, 2.5, 3.0]
-    tf_list = [12.0, 8.0, 6.0, 4.8, 4.0]
+    freq_list = [1.0, 1.5, 2.0, 2.5, 3.0]
+    amp_list = []
 
-    # t_knots = np.arange(0, 7)
-    # freq_list = [3, 3, 2, 1.9, 1.9, 1.9, 1.9]
-    # spline = PchipInterpolator(x=t_knots, y=freq_list, extrapolate=False)
+    iterator = construct_iterator(freq_list=freq_list, amp_list=amp_list)
 
     window = SlidingWindow(window_length=0.2, init_zero=True)
     window.add_data(name="Ux")
+    window.add_data(name="Pd")
 
-    for it, (freq, tf) in enumerate(zip(frequencies, tf_list)):
+    for it, (freq, amp) in enumerate(iterator):
         ns_solver.reset(func_count=it)
         window.reset()
+        window.window_length = 1 / freq
         obs_exporter.clear()
-        sim_path = RESULTS_DIR_PATH / f"simu_f_{freq}"
+        sim_path = RESULTS_DIR_PATH / f"simu_f_{freq}_a_{amp}"
         ns_solver.results_dir_path = sim_path
+        initial_position = (ns_solver.obs_wrap.x[0], ns_solver.obs_wrap.y[0])
     
-        while (ns_solver.simu_wrap.time < tf):
+        while (compute_dist(ns_solver.obs_wrap, initial_position=initial_position) < 6):
             ns_solver.obs_wrap.swimming_frequency = (freq, 1)
+            reg_amp = smoothstep(t=ns_solver.simu_wrap.time, tsmooth=T_SMOOTH)
+            ns_solver.obs_wrap.maximum_amplitude = (reg_amp, 1)
             obs_exporter.append_obs_data(ns_solver.simu_wrap, ns_solver.obs_wrap)
             phase = get_phase(obs_exporter=obs_exporter)
-            ns_solver.step(phase=phase)
+            ns_solver.obs_wrap.phase = (phase, 1)
+            ns_solver.step()
 
             t = ns_solver.simu_wrap.time
             Ux = ns_solver.obs_wrap.velocity_x[0]
-            other_data = {"Ux": Ux}
+            Pd = ns_solver.obs_wrap.power_def[0]
+            other_data = {"Ux": Ux, "Pd": Pd}
             window.append(t=t, other_data=other_data)
 
             
         obs_num = ns_solver.obs_wrap.obstacles_number
         obs_data = obs_exporter.obs_data
-        columns = ["time", "frequency", "x", "y", "Ux", "Uy", "Fx", "Fy", "density", "surface_area"]
+        columns = ["time", "frequency", "amplitude", "x", "y", "Ux", "Uy", "Fx", "Fy", "Pd", "surface_area"]
 
         if RANK==0:
             data_frames = []
@@ -89,11 +92,11 @@ if __name__ == "__main__":
             
             with open(sim_path / "ux_avg.pkl", "wb") as f:
                 pickle.dump(obj=window.data_set_avg["Ux"], file=f)
+
+            with open(sim_path / "pd_avg.pkl", "wb") as f:
+                pickle.dump(obj=window.data_set_avg["Pd"], file=f)
             
-            # jnb = JenksNaturalBreaks(N_CLUSTERS)
-            # jnb.fit(window.data_set_avg["Ux"])
-            # centroids = np.array(list(map(lambda x: np.mean(x), jnb.groups_)))
-            # np.save(file=sim_path / "centroids.npy", arr=np.flip(centroids))
+
             
             
 
